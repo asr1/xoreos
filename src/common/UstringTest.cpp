ec10c2528e09b388ddb13e1c6f1fb4b337169c1c
asr1 <alexandersrinehart@gmail.com>
2015-08-30 14:36:27 -0500

    Move xmlfix to Aurora namespace

---
+++ src/common/UstringTest.cpp
@@ -0,0 +1,17 @@
+#include <cstdarg>
+#include <cstdio>
+#include <cctype>
+
+#include "ustring.h"
+#include<iostream>
+
+
+using namespace std;
+
+namespace Common {
+
+void main(){
+cout<<"test";
+
+}
+}
